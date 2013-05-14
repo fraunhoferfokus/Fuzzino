1ead309d92eba708d4c5cbb1987e58e32c96a7f8
Martin Schneider <martin.schneider@fokus.fraunhofer.de>
2013-05-14 08:25:24 +0200

    updated Java examples

--- doc/examples/Java/JavaHeuristicExample.java
+++ doc/examples/Java/JavaHeuristicExample.java
@@ -9,14 +9,22 @@ import de.fraunhofer.fokus.fuzzing.fuzzino.request.java.StringType;
 public class JavaHeuristicExample {
 
 	public static void main(String[] args) {
+		// generate a a specification that describes the type (corresponding XML tag 'specification')
 		StringSpecification stringSpecification = RequestFactory.INSTANCE.createStringSpecification();
 		stringSpecification.setEncoding(StringEncoding.UTF8);
 		stringSpecification.setType(StringType.FILENAME);
 		stringSpecification.setIgnoreLengths(true);
 
+		// create instance of fuzzing heuristic for bad filenames
 		BadFilenamesGenerator badFilenamesGenerator = StringGeneratorFactory.INSTANCE.createBadFilenames(stringSpecification, 0);
+		
+		// fetch as many fuzzed values from the fuzzing heuristics as you need
 		for (FuzzedValue<String> fuzzedValue : badFilenamesGenerator) {
 			System.out.println(fuzzedValue);
+			
+			// decode control characters as needed
+			// the encoding scheme is \xnn for an ASCII character, \\unnnn for a UTF-16 character and \\unnnnnnnn for a UTF-32 character
+			// n is a hexadecimal digit
 			String decodedValue = fuzzedValue.getValue().replaceAll("\\\\x00", "" + ((char)0));
 			System.out.println(decodedValue);
 		}
--- doc/examples/Java/JavaXmlStringExample.java
+++ doc/examples/Java/JavaXmlStringExample.java
@@ -4,6 +4,8 @@ import de.fraunhofer.fokus.fuzzing.fuzzino.RequestDispatcher;
 public class JavaXmlStringExample {
 	
 	public static void main(String[] args) {
+		// generate a string containing an XML request document
+		// the fuzzingRequest.xsd defines a schema for valid request documents, the documentation contains its description
 		String xmlRequest = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
 				"<request xmlns=\"http://fuzzino.fuzzing.fokus.fraunhofer.de/request\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n\t" +
 				"xsi:schemaLocation=\"http://fuzzino.fuzzing.fokus.fraunhofer.de/request\">\n\n" +
@@ -17,10 +19,13 @@ public class JavaXmlStringExample {
 				"</string>\n\n" +
 				"</request>";
 		
+		// create instance of fuzzino
 		RequestDispatcher fuzzino = new RequestDispatcher(xmlRequest);
 		
+		// start processing
 		fuzzino.dispatch();
 		
+		// fetch the response of fuzzino
 		String xmlResponse = fuzzino.getXmlResponseString();
 		
 		System.out.println(xmlResponse);
